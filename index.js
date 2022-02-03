3a563b76037f87a4f2fff849b9673c7747d11959
Jack Krejci <jkrejci1988@gmail.com>
2022-02-02 19:51:21 -0600

    Fixed the expression tester up

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-ECHO is on.
+
+function testExpression() {
+    const usrTxt = document.getElementById("usrstr").value;
+    const regTxt = new RegExp(document.getElementById("regexp").value);
+
+    console.log(regTxt);
+    //Test the regular expression
+    check = regTxt.test(String(usrTxt));
+    console.log(check);
+    document.getElementById("check").innerHTML = check;
+    
+}
